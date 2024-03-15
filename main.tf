822168d87a490eea88af489df79b0da3a9e09c4b
samdroberts87 <127436118+samdroberts87@users.noreply.github.com>
2024-03-15 14:46:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "app_server" {
   ami             = "ami-08f7912c15ca96832"
   instance_type   = "t2.micro"
   key_name        = "Pipeline"  # Ensure this matches the case of the actual key pair name in AWS
-  security_groups = ["sg-02dd272262f14a262"]  # Ensure the security group ID is enclosed in double quotes
+  vpc_security_groups_ids = ["sg-02dd272262f14a262"]
 
   tags = {
     Name = "snake_game_server"
