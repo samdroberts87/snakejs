ca75e22f76f9f1c5a06c0fac10fddfe514552ff8
samdroberts87 <127436118+samdroberts87@users.noreply.github.com>
2024-03-15 11:46:56 +0000

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08f7912c15ca96832"
+  instance_type = "t2.micro"
+  key_name      = "pipeline"
+
+  tags = {
+    Name = "snake_game_server"
+  }
+}
+
+output "Name" {
+  value = aws_instance.app_server.tags.Name
+}
+
+output "public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+resource "null_resource" "save_output_to_file" {
+  provisioner "local-exec" {
+    command = "echo \"server_name=${aws_instance.app_server.tags.Name} server_IP=${aws_instance.app_server.public_ip} ssh_private_key_file=pipeline.pem\" > inventory.ini"
+  }
+}
+
